865d24e0e9793065fb57b3416b0753c2289eb8ec
Sheliya Mohsinali <sheliyamohsin342@gmail.com>
2026-09-21 12:45:16 +0530

    auto-save: progress update (21-Sep-2026 12:45:16 PM)

--- sku_manager/urls.py
+++ sku_manager/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.sku_dashboard, name='sku_dashboard'),
     path('edit/<int:pk>/', views.sku_edit, name='sku_edit'),
     path('delete/<int:pk>/', views.sku_delete, name='sku_delete'),
-    path('toggle-listed/<int:pk>/', views.toggle_listed, name='sku_toggle_listed'),  # <-- Added
+    path('toggle-listed/<int:pk>/', views.toggle_listed, name='sku_toggle_listed'),
+    path('stock/<int:pk>/<str:action>/', views.stock_adjust, name='sku_stock_adjust'),
     path('export-csv/', views.export_csv, name='sku_export_csv'),
 ]
